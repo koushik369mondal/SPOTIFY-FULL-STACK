06fdad1e6dd79601450d3249f7ecf76e59567b51
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-30 20:57:04 +0530

    add playlist creation prompt to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,6 +25,11 @@ const Sidebar = () => {
                         <img className='w-5' src={assets.plus_icon} alt="" />
                     </div>
                 </div>
+                <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
+                    <h1>Create your first playlist</h1>
+                    <p className='font-light'>Its easy we will help you</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+                </div>
             </div>
         </div>
     )
