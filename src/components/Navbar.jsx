ee1d2b4953a2482df80c6452be04c3c11a50e0ff
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-31 00:22:29 +0530

    add category buttons to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,6 +15,11 @@ const Navbar = () => {
                     <p className='bg-purple-500 text-black w-7 rounded-full flex items-center justify-center cursor-pointer'>K</p>
                 </div>
             </div>
+            <div className='flex items-center gap-2 mt-4'>
+                <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
+                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
+                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
+            </div>
         </>
     )
 }
