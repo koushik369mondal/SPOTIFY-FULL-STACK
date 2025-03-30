ce52a876b96403f19173cd24ad22cdac5ba5be80
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-31 00:17:07 +0530

    update Navbar component with new buttons for app installation and premium exploration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,13 @@ const Navbar = () => {
         <>
             <div className='w-full flex justify-between items-center font-semibold'>
                 <div className='flex items-center gap-2'>
-                    <img className='w-8 bg-black p-22 rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
-                    <img className='w-8 bg-black p-22 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
+                    <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
+                    <img className='w-8 bg-black p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
+                </div>
+                <div className='flex items-center gap-4'>
+                    <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore premium</p>
+                    <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
+                    <p className='bg-purple-500 text-black w-7 rounded-full flex items-center justify-center cursor-pointer'>K</p>
                 </div>
             </div>
         </>
