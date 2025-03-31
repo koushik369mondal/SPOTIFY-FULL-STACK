69d8115b6434703265aaf4525dd78245f3f8b2a3
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-31 10:49:00 +0530

    enhance DisplayAlbum component with detailed album information and improved layout

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData } from '../assets/frontend-assets/assets';
+import { albumsData, assets } from '../assets/frontend-assets/assets';
 
 const DisplayAlbum = () => {
 
-    const {id} = useParams();
+    const { id } = useParams();
     const albumData = albumsData[id];
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
-                <img src={albumData.image} alt="" />
+                <img className='w-48 rounded' src={albumData.image} alt="" />
+                <div className='flex flex-col'>
+                    <p>Playlist</p>
+                    <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+                    <h4>{albumData.desc}</h4>
+                    <p className='mt-1'>
+                        <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
+                        <b>Spotify</b>
+                        • 1,323, 154 likes
+                        • <b>50 songs,</b>
+                        about 2 hour 30 min
+                    </p>
+                </div>
             </div>
         </>
     )
