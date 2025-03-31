6f0f815495d2a4a0c8e2b56ca121d42dc564e340
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-31 18:03:49 +0530

    update PlayerContext to display seek bar width based on current audio time

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -36,6 +36,7 @@ const PlayerContextProvider = (props) => {
         setTimeout(() => {
 
             audioRef.current.ontimeupdate = () => {
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration * 100)) + '%';
                 setTime({
                     currentTime: {
                         second: Math.floor(audioRef.current.currentTime % 60),
