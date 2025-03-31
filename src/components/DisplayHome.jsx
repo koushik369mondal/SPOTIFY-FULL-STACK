d31d72f470b4901bc00473602c056552a78d018f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-31 08:35:36 +0530

    add featured charts header and improve album display layout in DisplayHome

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -8,7 +8,10 @@ const DisplayHome = () => {
         <>
         <Navbar/>
         <div className='mb-4'>
-            {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+            <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+            <div className='flex overflow-auto'>
+                {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+            </div>
         </div>
         </>
     )
