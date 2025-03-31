1d84beedd05b2375c61fe8063596c7c5e907b99f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-31 08:32:24 +0530

    add AlbumItem component and integrate into DisplayHome for album display

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/frontend-assets/assets'
+import AlbumItem from './AlbumItem'
 
 const DisplayHome = () => {
     return (
         <>
         <Navbar/>
+        <div className='mb-4'>
+            {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
         </>
     )
 }
