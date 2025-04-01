5d4a336e42041cc0a30c20fc34dcb9e9b2fc07fd
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-02 00:24:35 +0530

    add song routes and controller for managing songs

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import songRouter from './src/routes/songRoute.js';
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
+
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
 //initializing api routes
+app.use("/api/song", songRouter)
 app.get('/', (req, res) => res.send("API Working"))
 
 app.listen(port, ()=> console.log(`Server is started on ${port}`))
