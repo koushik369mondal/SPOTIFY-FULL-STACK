47fd4667b3a519456699948ba2dbeb7fcb7f11a6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-10 01:58:58 +0530

    refactor addSong function for improved readability and structure

--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -1,25 +1,47 @@
-import {v2 as cloudinary} from 'cloudinary';
-import songModel from '../models/songModel.js';
+import { v2 as cloudinary } from "cloudinary";
+import songModel from "../models/songModel.js";
 
 const addSong = async (req, res) => {
     try {
         const name = req.body.name;
         const desc = req.body.desc;
         const album = req.body.album;
-        const audioFile = req.files.audio[0]; 
+        const audioFile = req.files.audio[0];
         const imageFile = req.files.image[0];
-        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: 'video'});
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: 'image'});
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {
+            resource_type: "video",
+        });
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+            resource_type: "image",
+        });
+        const duration = `${Math.floor(audioUpload.duration / 60)}:${audioUpload.duration % 60
+            }`;
 
-        console.log(name, desc, album, audioUpload, imageUpload);
+        const songData = {
+            name,
+            desc,
+            album,
+            image: imageUpload.secure_url,
+            file: audioUpload.secure_url,
+            duration,
+        };
 
+        const song = songModel(songData);
+        await song.save();
+
+        res.json({ success: true, message: "Song added successfully" });
     } catch (error) {
-        
+        res.json({ success: false });
     }
-}
+};
 
-const listSong = async (req, res) => {
+const listSong = async (req, res) => { 
+    try {
+        const allSongs = await songModel.find({});
+        res.json({ success: true, songs: allSongs });
+    } catch (error) {
+        res.json({ success: false });
+    }
+};
 
-}
-
-export { addSong, listSong }
+export { addSong, listSong };
