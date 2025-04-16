bfecae6fd22d301cfcd4f8762c0c20ab069397af
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-16 13:45:24 +0530

    add form submission handler and update file input handling in AddSong component

--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -11,21 +11,25 @@ const AddSong = () => {
     const [loading, setLoading] = useState(false);
     const [albumDate, setAlbumDate] = useState([]);
 
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <form className='flex flex-col items-start gap-8 text-gray-600'>
+        <form onSubmit={onSubmitHandler} className='flex flex-col items-start gap-8 text-gray-600'>
             <div className='flex gap-8'>
                 <div className='flex flex-col gap-4'>
                     <p>Upload Song</p>
-                    <input type="file" id='song' accept='audio/*' hidden />
+                    <input onChange={(e) => setSong(e.target.files[0])} type="file" id='song' accept='audio/*' hidden />
                     <label htmlFor="song">
-                        <img src={assets.upload_song} className='w-24 cursor-pointer' alt="" />
+                        <img src={song ? assets.upload_added : assets.upload_song} className='w-24 cursor-pointer' alt="" />
                     </label>
                 </div>
                 <div className='flex flex-col gap-4'>
                     <p>Upload Image</p>
-                    <input type="file" id='image' accept='image/*' hidden />
+                    <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' accept='image/*' hidden />
                     <label htmlFor="image">
-                        <img src={assets.upload_area} className='w-24 cursor-pointer' alt="" />
+                        <img src={image ? URL.createObjectURL(image) : assets.upload_area} className='w-24 cursor-pointer' alt="" />
                     </label>
                 </div>
             </div>
