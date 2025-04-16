14db2cf9558da4c69a3e596d26b3f1ab207a8d8a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-16 12:14:09 +0530

    add AddSong component with file upload functionality for songs and images

--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const AddSong = () => {
     return (
-        <div>
-            
-        </div>
+        <form className='flex flex-col items-start gap-8 text-gray-600'>
+            <div className='flex gap-8'>
+                <div className='flex flex-col gap-4'>
+                    <p>Upload Song</p>
+                    <input type="file" id='song' accept='audio/*' hidden />
+                    <label htmlFor="song">
+                        <img src={assets.upload_song} className='w-24 cursor-pointer' alt="" />
+                    </label>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    <p>Upload Image</p>
+                    <input type="file" id='image' accept='image/*' hidden />
+                    <label htmlFor="image">
+                        <img src={assets.upload_area} className='w-24 cursor-pointer' alt="" />
+                    </label>
+                </div>
+            </div>
+        </form>
     )
 }
 
