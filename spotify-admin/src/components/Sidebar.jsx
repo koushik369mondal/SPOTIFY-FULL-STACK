c67c5f59381ce011adf429fd1c03f5a3e5cddc52
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-16 11:55:42 +0530

    add navigation items for song and album management in Sidebar component

--- spotify-admin/src/components/Sidebar.jsx
+++ spotify-admin/src/components/Sidebar.jsx
@@ -8,6 +8,30 @@ const Sidebar = () => {
             <img src={assets.logo} className='mt-5 w [max(100vw, 100px)] hidden sm:block' alt="" />
             <img src={assets.logo_small} className='mt-5 w-[max(5vw, 40px)] mr-5 sm:hidden block' alt="" />
 
+            <div className='flex flex-col gap-5 mt-10'>
+
+                <div className='flex items-center gap-2.5 text-gray-800 bg-white border-black p-2 pr-[max(8vw, 10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+                    <img src={assets.add_song} className='w-5' alt="" />
+                    <p className='hidden sm:block'>Add Song</p>
+                </div>
+
+                <div className='flex items-center gap-2.5 text-gray-800 bg-white border-black p-2 pr-[max(8vw, 10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+                    <img src={assets.song_icon} className='w-5' alt="" />
+                    <p className='hidden sm:block'>List Song</p>
+                </div>
+
+                <div className='flex items-center gap-2.5 text-gray-800 bg-white border-black p-2 pr-[max(8vw, 10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+                    <img src={assets.add_album} className='w-5' alt="" />
+                    <p className='hidden sm:block'>Add Album</p>
+                </div>
+
+                <div className='flex items-center gap-2.5 text-gray-800 bg-white border-black p-2 pr-[max(8vw, 10px)] drop-shadow-[-4px_4px_#00FF5B] text-sm font-medium'>
+                    <img src={assets.album_icon} className='w-5' alt="" />
+                    <p className='hidden sm:block'>List Album</p>
+                </div>
+
+            </div>
+
         </div>
     )
 }
