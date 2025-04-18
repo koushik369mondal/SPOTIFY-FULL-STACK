58d1f58b23a5e595ccb8f0a279e7d40663aa9e9f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-18 13:21:55 +0530

    Refactor AddAlbum component for improved loading state handling and code formatting

--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -50,7 +50,6 @@ const AddSong = () => {
     return  loading ? (
         <div className='grid place-items-center min-h-[80vh]'>
             <div className='w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin'>
-
             </div>     
         </div>
     ) : (
