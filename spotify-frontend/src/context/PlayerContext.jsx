dde5ba649184a139e9f9ea3c3778a7822e6b4849
Kaushik Mandal <137462541+koushik369mondal@users.noreply.github.com>
2025-04-23 15:40:55 +0530

    Update PlayerContext.jsx

--- spotify-frontend/src/context/PlayerContext.jsx
+++ spotify-frontend/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
     const seekBg = useRef();
     const seekBar = useRef();
 
-    const url = 'http://localhost:4000';
+    const url = 'https://spotify-backend-11t0.onrender.com';
 
     const [songsData, setSongsData] = useState([]);
     const [albumsData, setAlbumsData] = useState([]);
